154ba3510790ab6be4d894137091e5343c212bbf
gunjan0115 <gunjangikwad@gmail.com>
2026-08-20 23:52:00 +0530

    pre-order is almost done

--- js/firebase.js
+++ js/firebase.js
@@ -2,6 +2,7 @@
 // FIREBASE CONFIGURATION
 // =========================================
 
+
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
 
 import { getAuth } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
